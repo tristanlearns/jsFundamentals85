4679a55ea959c3f1030c87db6038e7dcb65c0391
Tristan Smith <tristan.smith@gmail.com>
2021-05-07 14:45:38 -0400

    lots of new saves

---
+++ arrays/bracketNotation.js
@@ -0,0 +1,9 @@
+let list = ["orange", "banana", "oreos"];
+
+console.log(list[2]);
+
+let students = ["Michelle", "Raheem", "Adam", "Alyssa"];
+
+console.log(students[3]);
+
+console.log(`Hello ${students[1]}!`);
